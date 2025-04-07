7255ce9fc205399a201bd350ee32604f228d5524
zelhs <antreaszel@gmail.com>
2025-04-07 23:59:30 +0300

    3rd change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>This is a change x2!</h1>')
+  res.send('<h1>This is a change x3!</h1>')
 })
 
 const PORT = 8080
